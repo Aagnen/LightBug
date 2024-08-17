0e527f5c2bc53f4436ef538fe8d83ecd588d3e4c
Agnieszka Nowacka <agnieszka.nowacka@object.pl>
2024-08-17 17:29:07 +0200

    DiffGrowth wip

---
+++ LightBug.Core/DifferentialGrowth/MeshSystem.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Plankton; //AddReference to the project -> browse -> find file in Gh libraries
+using PlanktonGh;
+using Rhino.Geometry;
+
+
+namespace LightBug.Core.DifferentialGrowth
+{
+    public class MeshSystem
+    {
+        public PlanktonMesh PlanktonMesh = null;
+
+        private List<Vector3d> totalWeightedMoves;
+        private List<double> totalWeights;
+
+        public MeshSystem(Mesh startingMesh)
+        {
+            PlanktonMesh = startingMesh.ToPlanktonMesh();
+        }
+
+        public Mesh GetRhinoMesh()
+        {
+            return PlanktonMesh.ToRhinoMesh();
+        }
+
+        private void Update(
+            Point3d Attractor,
+            double MaxDistFromAttractor,
+            Mesh StartingMesh,
+            bool Grow,
+            int MaxVertexCount,
+            double EdgeLengthConstraintWeight,
+            double CollisionDistance,
+            double CollisionWeight,
+            double BendingResistanceWeight,
+            ref object Mesh)
+        {
+            if (Grow)
+                SplitAllLongEdges(MaxVertexCount, CollisionDistance);
+
+            totalWeightedMoves = new List<Vector3d>();
+            totalWeights = new List<double>();
+
+            for (int i = 0; i < PlanktonMesh.Vertices.Count; i++)
+            {
+                totalWeightedMoves.Add(new Vector3d(0, 0, 0));
+                totalWeights.Add(0.0);
+            }
+
+            ProcessCollisionsUsingRTree(CollisionDistance, CollisionWeight);
+
+            ProcessBendingResistance(BendingResistanceWeight);
+            ProcessEdgeLengthConstraint(CollisionDistance, EdgeLengthConstraintWeight);
+
+            ProcessAttractor(Attractor, MaxDistFromAttractor);
+
+            UpdateVertexPositions();
+            Mesh = PlanktonMesh.ToRhinoMesh();
+        }
+}
--- LightBug.Gh/GhComponent.cs
+++ LightBug.Gh/GhComponent.cs
@@ -1,63 +1,90 @@
-//using Grasshopper;
-//using Grasshopper.Kernel;
-//using Rhino.Geometry;
-//using System;
-//using System.Collections.Generic;
+using System;
+using System.Collections.Generic;
+using Grasshopper;
+using Grasshopper.Kernel;
+using Rhino.Geometry;
+using LightBug.Core.DifferentialGrowth;
 
-//namespace LightBug.Gh
-//{
-//    public class GhComponent : GH_Component
-//    {
-//        /// <summary>
-//        /// Each implementation of GH_Component must provide a public 
-//        /// constructor without any arguments.
-//        /// Category represents the Tab in which the component will appear, 
-//        /// Subcategory the panel. If you use non-existing tab or panel names, 
-//        /// new tabs/panels will automatically be created.
-//        /// </summary>
-//        public GhComponent()
-//          : base("GhComponent", "Nickname",
-//            "Description",
-//            "Category", "Subcategory")
-//        {
-//        }
+namespace LightBug.Gh
+{
+    public class MeshDifferentialGrowth : GH_Component
+    {
+        public MeshDifferentialGrowth()
+            : base(
+                "MeshDifferentialGrowth",
+                "MeshDifferentialGrowth",
+                "Expand a mesh based on subdivision and avoiding self-collision. Based on YT tutorials by Long Nguyen and then modified.",
+                "Lightbug",
+                "DifferentialGrowth")
+        {
+        }
 
-//        /// <summary>
-//        /// Registers all the input parameters for this component.
-//        /// </summary>
-//        protected override void RegisterInputParams(GH_Component.GH_InputParamManager pManager)
-//        {
-//        }
+        protected override System.Drawing.Bitmap Icon => null;
 
-//        /// <summary>
-//        /// Registers all the output parameters for this component.
-//        /// </summary>
-//        protected override void RegisterOutputParams(GH_Component.GH_OutputParamManager pManager)
-//        {
-//        }
+        public override Guid ComponentGuid => new Guid("0bc9e2cd-0684-40ce-a2b4-1aedb3c9f4be");
 
-//        /// <summary>
-//        /// This is the method that actually does the work.
-//        /// </summary>
-//        /// <param name="DA">The DA object can be used to retrieve data from input parameters and 
-//        /// to store data in output parameters.</param>
-//        protected override void SolveInstance(IGH_DataAccess DA)
-//        {
-//        }
+        protected override void RegisterInputParams(GH_Component.GH_InputParamManager pManager)
+        {
+            pManager.AddBooleanParameter("Reset", "Reset", "Reset", GH_ParamAccess.item);
+            pManager.AddMeshParameter("StartingMesh", "M", "StartingMesh", GH_ParamAccess.item);
+            pManager.AddBooleanParameter("Grow", "Grow", "Grow", GH_ParamAccess.item);
 
-//        /// <summary>
-//        /// Provides an Icon for every component that will be visible in the User Interface.
-//        /// Icons need to be 24x24 pixels.
-//        /// You can add image files to your project resources and access them like this:
-//        /// return Resources.IconForThisComponent;
-//        /// </summary>
-//        protected override System.Drawing.Bitmap Icon => null;
+            pManager.AddPointParameter("AttractorPoint", "Attractor", "", GH_ParamAccess.item);
+            pManager.AddNumberParameter("MaxDistanceFromAttractor", "MaxDist", "", GH_ParamAccess.item);
 
-//        /// <summary>
-//        /// Each component must have a unique Guid to identify it. 
-//        /// It is vital this Guid doesn't change otherwise old ghx files 
-//        /// that use the old ID will partially fail during loading.
-//        /// </summary>
-//        public override Guid ComponentGuid => new Guid("0bc9e2cd-0684-40ce-a2b4-1aedb3c9f4be");
-//    }
-//}
+            pManager.AddNumberParameter("EdgeLengthConstraintWeight", "EdgeLenWeight", "", GH_ParamAccess.item);
+            pManager.AddNumberParameter("CollisionDistance", "CollDist", "", GH_ParamAccess.item);
+            pManager.AddNumberParameter("CollisionWeight", "CollWeight", "", GH_ParamAccess.item);
+            pManager.AddNumberParameter("BendingResistanceWeight", "BendingResWeight", "t", GH_ParamAccess.item);
+
+            pManager.AddIntegerParameter("Max. Vertex Count", "Max. Vertex Count", "Max. Vertex Count", GH_ParamAccess.item, 3000);
+        }
+
+        protected override void RegisterOutputParams(GH_Component.GH_OutputParamManager pManager)
+        {
+            pManager.AddMeshParameter("Mesh", "Mesh", "Mesh", GH_ParamAccess.item);
+        }
+
+        private MeshSystem myMeshGrowthSystem;
+
+        protected override void SolveInstance(IGH_DataAccess DA)
+        {
+            bool iReset = true;
+            Mesh iStartingMesh = null;
+            bool iGrow = false;
+
+            Point3d attractor = new Point3d();
+            double maxDistFromAttractor = 0;
+
+            double iEdgeLengthConstrainWeight = 0.0;
+            double iCollisionDistance = 0.0;
+            double iCollisionWeight = 0.0;
+            double iBendingResistanceWeight = 0.0;
+
+            int iMaxVertexCount = 0;
+
+            DA.GetData("Reset", ref iReset);
+            DA.GetData("Starting Mesh", ref iStartingMesh);
+            DA.GetData("Grow", ref iGrow);
+            DA.GetData("Max. Vertex Count", ref iMaxVertexCount);
+            DA.GetData("Edge Length Constraint Weight", ref iEdgeLengthConstrainWeight);
+            DA.GetData("Collision Distance", ref iCollisionDistance);
+            DA.GetData("Collision Weight", ref iCollisionWeight);
+            DA.GetData("Bending Resistance Weight", ref iBendingResistanceWeight);
+
+            if (iReset || myMeshGrowthSystem == null)
+                myMeshGrowthSystem = new MeshSystem(iStartingMesh);
+
+            myMeshGrowthSystem.Grow = iGrow;
+            myMeshGrowthSystem.MaxVertexCount = iMaxVertexCount;
+            myMeshGrowthSystem.EdgeLengthConstrainWeight = iEdgeLengthConstrainWeight;
+            myMeshGrowthSystem.CollisionWeight = iCollisionWeight;
+            myMeshGrowthSystem.BendingResistanceWeight = iBendingResistanceWeight;
+            myMeshGrowthSystem.CollisionDistance = iCollisionDistance;
+
+            myMeshGrowthSystem.Update();
+
+            DA.SetData("Mesh", myMeshGrowthSystem.GetRhinoMesh());
+        }
+    }
+}
